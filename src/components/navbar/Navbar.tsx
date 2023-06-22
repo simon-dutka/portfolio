7fc4b69d8a5cc2ed1d19e6e30d9dbe1207e25219
Simon Dutka <SimonDutkaWork@gmail.com>
2023-06-22 22:07:46 +0200

    Added bottom navbar for mobile
    -Added navbar icons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/components/navbar/navbar.scss';
+import { isMobile } from 'react-device-detect';
 
 // Images
 import sun from '../../assets/sun.svg';
+
+// Icons
+import about from '../../assets/navbar_icons/about.svg';
+import tech from '../../assets/navbar_icons/tech.svg';
+import projects from '../../assets/navbar_icons/projects.svg';
+import resume from '../../assets/navbar_icons/resume.svg';
+import contact from '../../assets/navbar_icons/contact.svg';
 
 const NavbarContent = () => {
     return (
@@ -17,17 +25,61 @@ const NavbarContent = () => {
 };
 
 function Navbar() {
+    const iconsNavbar = [
+        {
+            src: about,
+            alt: 'item',
+        },
+        {
+            src: tech,
+            alt: 'item',
+        },
+        {
+            src: projects,
+            alt: 'item',
+        },
+        {
+            src: resume,
+            alt: 'item',
+        },
+        {
+            src: contact,
+            alt: 'item',
+        },
+    ];
+
+    const [resolution, setResolution] = useState(window.innerWidth);
+
+    const checkMobile = () => {
+        setResolution(window.innerWidth);
+    };
+
+    window.addEventListener('resize', checkMobile);
+
     return (
         <>
-            <nav className='navbar'>
-                <img
-                    src={sun}
-                    alt='Sun - light theme icon'
-                    className='navbar__theme'
-                />
+            {resolution < 1000 ? (
+                <nav className='navbar__bottom'>
+                    {iconsNavbar.map((el, id) => (
+                        <img
+                            className='navbar__bottom-icon'
+                            src={el.src}
+                            alt={el.alt}
+                            key={id}
+                        />
+                    ))}
+                </nav>
+            ) : (
+                <nav className='navbar'>
+                    <img
+                        src={sun}
+                        alt='Sun - light theme icon'
+                        className='navbar__theme'
+                    />
 
-                <NavbarContent />
-            </nav>
+                    <NavbarContent />
+                </nav>
+            )}
         </>
     );
 }
