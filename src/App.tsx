91a140f67712db7505a57128fe580dc6794244c5
Simon Dutka <SimonDutkaWork@gmail.com>
2023-07-10 16:00:15 +0200

    Added contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FullPage, Slide } from 'react-full-page';
 import { Main, About, Projects, Resume } from './pages/index';
+import { Main, About, Projects, Resume, Contact } from './pages/index';
 import { Page, Level } from './components/index';
 
 function App() {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,3 +2,4 @@ export { default as Main } from './main/Main';
 export { default as About } from './about/About';
 export { default as Resume } from './resume/Resume';
 export { default as Projects } from './projects/Projects';
+export { default as Contact } from './contact/Contact';
