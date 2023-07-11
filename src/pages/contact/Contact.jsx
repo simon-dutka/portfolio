d46468e89fedf8dcbe3d4d93e24c65de930b4968
Simon Dutka <SimonDutkaWork@gmail.com>
2023-07-11 22:29:07 +0200

    Updated 'contact' section

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,63 @@
 import React from 'react';
 import '../../styles/pages/contact/contact.scss';
 
+import outline from '../../assets/outline.svg';
+
 function Contact() {
-    return <></>;
+    return (
+        <section className='contact content contact__content'>
+            <h2 className='contact__heading'>Contact</h2>
+
+            <p className='contact__text'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                porttitor tellus aliquet enim laoreet, a suscipit est auctor.
+                Sed rhoncus posuere
+            </p>
+
+            <form className='contact__form' action='' method='post'>
+                <div className='contact__form-first-line'>
+                    <input
+                        className='contact__form-input contact__form-input--short'
+                        type='text'
+                        name='firstName'
+                        placeholder='First name'
+                    />
+
+                    <input
+                        className='contact__form-input contact__form-input--short'
+                        type='text'
+                        name='lastName'
+                        placeholder='Last name'
+                    />
+                </div>
+
+                <input
+                    className='contact__form-input'
+                    type='email'
+                    name='email'
+                    placeholder='Email'
+                />
+
+                <input
+                    className='contact__form-input'
+                    type='message'
+                    name='message'
+                    placeholder='Message'
+                />
+
+                <div className='contact__form-button-container'>
+                    <img
+                        className='contact__form-button-image'
+                        src={outline}
+                        alt=''
+                    />
+                    <button className='contact__form-button' type='submit'>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
 }
 
 export default Contact;
